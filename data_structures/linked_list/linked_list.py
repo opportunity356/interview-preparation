7679865107f806dcce9d29839ba1147d2aaad013
opportunity356 <opportunity356@yandex.ru>
2016-05-22 10:40:40 +0300

    refactored LinkedList class

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -6,10 +6,10 @@ __author__ = 'opportunity356'
 import random
 
 
-class Node(object):
+class ListNode(object):
 
-    def __init__(self, data, next_node=None):
-        self.data = data
+    def __init__(self, value, next_node=None):
+        self.value = value
         self.next = next_node
 
 
@@ -18,22 +18,33 @@ class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
 
-    def print_list(self):
-        curr_elem = self.head
+        self.tail = self.head
+        if self.tail:
+            while self.tail.next:
+                self.tail = self.tail.next
 
-        while curr_elem:
-            print curr_elem.data
-            curr_elem = curr_elem.next
+    def add(self, value):
+        self.tail.next = ListNode(value)
+
+    def __str__(self):
+        list_items = []
+
+        curr_node = self.head
+        while curr_node:
+            list_items.append(str(curr_node.value))
+            curr_node = curr_node.next
+
+        return ' -> '.join(list_items)
 
 
 def generate_list_with_random_data(length, with_loop=False):
     n = random.randint(0, 1000)
-    _l = LinkedList(head=Node(n))
+    _l = LinkedList(head=ListNode(n))
     curr_elem = _l.head
 
     for _ in xrange(length - 1):
         n = random.randint(0, 100)
-        curr_elem.next = Node(n)
+        curr_elem.next = ListNode(n)
         curr_elem = curr_elem.next
 
     # if with_loop:
@@ -54,7 +65,7 @@ def has_loop(l):
         turtle = turtle.next
         rabbit = rabbit.next.next
 
-        print turtle.data, rabbit.data
+        print turtle.value, rabbit.value
 
         if turtle == rabbit:
             res = True
@@ -89,7 +100,7 @@ def is_poly(l):
     even = True
     while True:
 
-        stack.append(p1.data)
+        stack.append(p1.value)
         if not p2.next:
             even = False
             break
@@ -106,7 +117,7 @@ def is_poly(l):
 
     while p1.next:
         p1 = p1.next
-        if p1.data != stack.pop():
+        if p1.value != stack.pop():
             return False
 
     return True
@@ -119,7 +130,7 @@ def main():
     # l.print_list()
     #
     # print is_poly(l.head)
-    # print k_last_elem(l, 5).data
+    # print k_last_elem(l, 5).value
 
     # print has_loop(l)
     #
@@ -134,11 +145,11 @@ def main():
     #     curr_elem = curr_elem.next
     # curr_elem = tmp
     #
-    a = Node(3)
-    b = Node(2, next_node=a)
-    c = Node(7, next_node=b)
-    d = Node(2, next_node=c)
-    e = Node(3, next_node=d)
+    a = ListNode(3)
+    b = ListNode(2, next_node=a)
+    c = ListNode(7, next_node=b)
+    d = ListNode(2, next_node=c)
+    e = ListNode(3, next_node=d)
     ll = LinkedList(head=e)
 
     ll.print_list()
