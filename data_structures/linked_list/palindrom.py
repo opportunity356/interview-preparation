2be65d8925984f1b635d43cc7225733826a28f13
opportunity356 <opportunity356@yandex.ru>
2016-05-28 21:38:16 +0300

    is_palindrom function now works

--- data_structures/linked_list/palindrom.py
+++ data_structures/linked_list/palindrom.py
@@ -3,9 +3,11 @@
 
 __author__ = 'opportunity356'
 
+from linked_list import LinkedList, ListNode
+
 
 def is_palindrom(l):
-    p1 = p2 = l
+    p1 = p2 = l.head
     stack = []
     even = True
     while True:
@@ -20,10 +22,8 @@ def is_palindrom(l):
         p1 = p1.next
         p2 = p2.next.next
 
-        print stack
-
-        if not even:
-            stack.pop()
+    if not even:
+        stack.pop()
 
     while p1.next:
         p1 = p1.next
@@ -31,3 +31,16 @@ def is_palindrom(l):
             return False
 
     return True
+
+
+if __name__ == '__main__':
+    a = ListNode(3)
+    b = ListNode(2, next_node=a)
+    c = ListNode(7, next_node=b)
+    d = ListNode(2, next_node=c)
+    e = ListNode(3, next_node=d)
+    ll = LinkedList(head=e)
+
+    print ll
+
+    print is_palindrom(ll)
